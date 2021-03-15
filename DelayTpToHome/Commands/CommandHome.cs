fdef4556834f99f79581fcb7d10f815bb2d51ca9
CPL USA <CPL USA@DESKTOP-4D9RMEI>
2021-03-15 20:44:49 +0200

    Add project files.

---
+++ DelayTpToHome/Commands/CommandHome.cs
@@ -0,0 +1,61 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DelayTpToHome.Commands
+{
+    public class CommandHome : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "home";
+
+        public string Help => string.Empty;
+
+        public string Syntax => string.Empty;
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            bool result = false;
+
+            foreach (CSteamID item in DelayTpToHome.Instance.TeleportationPlayers)
+            {
+                if (item == player.CSteamID)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            if (result==false)
+            {
+                if (BarricadeManager.tryGetBed(player.CSteamID, out Vector3 point, out byte angle))
+                {
+                    point.y += 0.5f;
+                    DelayTpToHome.Instance.TeleportationPlayers.Add(player.CSteamID);
+                    DelayTpToHome.Instance.StartCoroutine(DelayTpToHome.Instance.HomeTeleportationInterval(player, point));
+                }
+                else
+                {
+                    UnturnedChat.Say(player, DelayTpToHome.Instance.Translate("command_home_not_found"), Color.red);
+                }
+            }
+            else
+            {
+                UnturnedChat.Say(player, DelayTpToHome.Instance.Translate("command_home_already_tp"), Color.yellow);
+            }
+        }
+    }
+}
---
+++ DelayTpToHome/DelayTpToHome.cs
@@ -0,0 +1,57 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DelayTpToHome
+{
+    public class DelayTpToHome : RocketPlugin
+    {
+        static public DelayTpToHome Instance;
+
+        public  List<CSteamID> TeleportationPlayers;
+
+        protected override void Load()
+        {
+            Instance = this;
+            TeleportationPlayers = new List<CSteamID>();
+        }
+
+        protected override void Unload()
+        {
+            
+        }
+
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            {"command_home_successfully", "Вы успешно телепортированы на спальник." },
+            {"command_home_cooldawn", "Вы будете телепортированы через 10 секунд." },
+            {"command_home_not_found", "Спальник не найден." },
+            {"command_home_already_tp", "Вы уже телепортируетесь." },
+        };
+
+
+        public IEnumerator HomeTeleportationInterval(UnturnedPlayer player, Vector3 position )
+        {
+            UnturnedChat.Say(player, Translate("command_home_cooldawn"), Color.yellow);
+            yield return new WaitForSeconds(10);
+            if (player.Player.teleportToLocation(position, player.Player.look.yaw))
+            {
+                UnturnedChat.Say(player, Translate("command_home_successfully"), Color.green);
+
+                TeleportationPlayers.Remove(player.CSteamID);
+            }
+        }
+
+    }
+}
